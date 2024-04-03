fd1ecf1530a7e18a3bbfbfd99bada220ea42e881
Tang <matthew.tang@stfc.ac.uk>
2024-04-03 11:12:01 +0100

    removed hard-coded file paths from single-molecule analysis

--- new_scripts/sm_example_analysis.py
+++ new_scripts/sm_example_analysis.py
@@ -11,13 +11,9 @@ project_name = input('Please enter project name: ')
 # Initialize sm data
 molecule1 = drift_analysis_script.SingleMoleculeData(project_name)
 
-data = 'C:/Users/mxq76232/Downloads/smlm_minimal_sample_drift_data/AF647_single_molecule_ROCS/5.csv'
+data = input('Please enter path to localisation data: ')
 
-#input('Please enter path to localisation data: ')
-
-out = 'C:/Users/mxq76232/Desktop'
-
-#input('Please enter output path: ')
+out = input('Please enter output path: ')
 
 print('Loaded!')
 
